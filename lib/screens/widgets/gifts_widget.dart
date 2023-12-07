8908ee71ebd6126d90614cd34c092c0a2f851086
Allen Silverio <allensilverioo>
2023-12-07 08:36:34 -0400

    Gift Chip

--- lib/screens/widgets/gifts_widget.dart
+++ lib/screens/widgets/gifts_widget.dart
@@ -5,6 +5,30 @@ class GiftsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Wrap(
+      spacing: 10.0,
+      runSpacing: 5.0,
+      children: [
+        _giftChip("Gift 1"),
+        _giftChip("Gift 1"),
+        _giftChip("Gift 1"),
+        _giftChip("Gift 1"),
+        _giftChip("Gift 1"),
+        _giftChip("Gift 1"),
+        _giftChip("Gift 1"),
+      ],
+    );
+  }
+
+  Chip _giftChip(String chipText) {
+    return Chip(
+        label: Text(chipText),
+        avatar: Icon(Icons.card_giftcard_rounded, color: Colors.blue.shade400, size: 20), 
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4.0),
+          side: BorderSide(color: Colors.grey),
+        ),
+        backgroundColor: Colors.grey.shade100,
+      );
   }
 }
